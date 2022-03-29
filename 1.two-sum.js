9cbfe5b1d0e6245ad7692f9b60b9a82710094bc7
Mariko Iwata <3701668+MarikoIwata@users.noreply.github.com>
2022-03-28 23:49:48 -0400

    "commiting my work in progress"

---
+++ 1.two-sum.js
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=1 lang=javascript
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */    
+var twoSum = function(nums, target) {
+    const result = [];
+    for (let i = 0; i<nums.length-1; i++){
+        for (let j = 1; i<nums.length; j++){
+            console.log(i,j)
+            if(nums[i]+nums[j] === target){
+                result.push(i, j)
+                return result
+            }
+        }
+    }
+    
+};
+
+console.log("hello")
+// @lc code=end
+
---
+++ test.js
@@ -0,0 +1,16 @@
+
+var twoSum = function(nums, target) {
+    const result = [];
+    for (let i = 0; i<nums.length-1; i++){
+        for (let j = 1; i<nums.length; j++){
+            console.log(i,j)
+            if(nums[i]+nums[j] === target){
+                result.push(i, j)
+                return result
+            }
+        }
+    }
+    
+}
+console.log("hi")
+console.log(twoSum([3,2,4], 6))//[1,2]
